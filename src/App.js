c6bb88e96f1d8829613fddccc24deb0abece9085
Phattarakorn Nalinbenchaphan <ackfort2547@gmail.com>
2024-06-11 14:48:45 +0700

    init MUI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
