64f26e0abdb1854882b6911b9ab7b12b810023d1
Phattarakorn Nalinbenchaphan <ackfort2547@gmail.com>
2024-06-11 16:16:43 +0700

    test Typography

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Delete, Send } from "@mui/icons-material";
 
 function App() {
@@ -10,6 +10,12 @@ function App() {
       <Button variant="contained" endIcon={<Send />}>
         Send
       </Button>
+      <Typography variant="h1" component="p">
+        h1. Heading p tag
+      </Typography>
+      <h1>
+        hello
+      </h1>
     </div>
   );
 }
